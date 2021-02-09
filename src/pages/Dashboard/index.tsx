b2573499dcde6948a36c8e355732e1b3ed3f39d5
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-08 22:09:20 -0300

    Listar os pratos de comida da sua API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -28,6 +28,9 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // TODO LOAD FOODS
+      api.get('/foods').then(response =>{
+        setFoods(response.data);
+      })
     }
 
     loadFoods();
